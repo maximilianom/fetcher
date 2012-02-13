1f16322979e62bfc294492ce7c477d220205f21e
Maximiliano Mendez <mmendez@kyheo.it.popego.com.ar>
2012-02-13 15:12:03 -0300

    Added empty Reader class for RabbitMQ

---
+++ RabbitMQReaderQueue.py
@@ -0,0 +1,5 @@
+import pika
+
+class RabbitMQReaderQueue(object):
+    """
+    """
---
+++ fetcher.py
@@ -0,0 +1,15 @@
+import sys
+
+import RabbitMQReaderQueue
+import RabbitMQWriterQueue
+import crawle
+import SaveInFileHandler
+
+class Fetcher(object):
+    """
+    """
+
+    def fetch(file):
+        urls = []
+
+        pass
