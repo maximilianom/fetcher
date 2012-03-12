dd0259dee23b8e38adaf02c4c742854b7a107938
mmendez <mmendez@mmendez-pc.(none)>
2012-03-12 15:03:44 -0300

    minor changes in template and urls

--- fetcher/process/urls.py
+++ fetcher/process/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('fetcher.process.views',
     url(r'^$', 'process', name='process'),
-    url(r'^add_machine/$', 'add_machine', name="add"),
-    url(r'^stop/$', 'stop', name="stop"),
+    url(r'^(?P<job_id>\d+)$', 'show_process', name="status"),
+    url(r'^(?P<job_id>\d+/add_machine/$', 'add_machine', name="add"),
+    url(r'^(?P<job_id>\d+/stop/$', 'stop', name="stop"),
     )
